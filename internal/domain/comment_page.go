e530e3f85b028f62857adebc0b53e291cbd0d703
Timofey Chernyshev <timkachern@mail.ru>
2026-05-31 11:28:12 +0300

    feat: remove CommentPage and PostPage from domain

--- internal/domain/comment_page.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import cursorpagination "github.com/TimofeyChernyshev/comment-post-system/pkg/cursor_pagination"
-
-type CommentPage struct {
-	Comments []*Comment
-	PageInfo *cursorpagination.PageInfo
-}
--- internal/domain/post_page.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import cursorpagination "github.com/TimofeyChernyshev/comment-post-system/pkg/cursor_pagination"
-
-type PostPage struct {
-	Posts    []*Post
-	PageInfo *cursorpagination.PageInfo
-}
